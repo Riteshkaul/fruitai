22474949e8939ae05a7c0f7508a1c10070173441
Ritesh <riteshkaul682@gmail.com>
2024-09-15 20:43:22 +0530

    new commit

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -23,10 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(rk^a!n$_@*u8q%l-p$ejfoptk9r!soudz3g6^34t&2ii&k+e@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['fruitai-1-0kzw.onrender.com', 'localhost', '127.0.0.1']
 
+# ALLOWED_HOSTS = ['fruitai-1-0kzw.onrender.com', 'localhost', '127.0.0.1']
+if os.getenv('RENDER'):  # Render environment
+    DEBUG = False
+    ALLOWED_HOSTS = ['fruitai-1-0kzw.onrender.com/']  # Update with your Render domain
+else:  # Local development
+    DEBUG = True
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
 # Application definition
 
